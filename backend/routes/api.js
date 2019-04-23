4d3e9ea7ffd1cba85580b7942a3f952fb0c639de
musashi.sakamoto <musashi.sakamoto@brilliantservice.co.jp>
2019-04-23 13:32:38 +0900

    ファイル名変更

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,9 @@
 const passport = require('passport');
 const router = require('express-promise-router')();
 
-const authController = require('../controllers/auth');
-const imageController = require('../controllers/images');
-const userController = require('../controllers/users');
+const authController = require('../controllers/authController');
+const imageController = require('../controllers/imagesController');
+const userController = require('../controllers/usersController');
 
 router.post('/login', authController.login);
 router.get('/logout', passport.authenticate('jwt', { session: false }), authController.logout);
