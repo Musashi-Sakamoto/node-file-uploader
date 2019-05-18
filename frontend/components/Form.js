c66ce71d4ff866363bdbf8aa3ff11d3cc8d73416
MusashiSakamoto <1292602b@gmail.com>
2019-05-18 13:23:12 +0900

    formを別コンポーネント化

---
+++ frontend/components/Form.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Router from 'next/router';
+import cookie from 'js-cookie';
+import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const styles = () => ({
+  container: {
+    display: 'flex',
+    paddingTop: 100
+  },
+  formContainer: {
+    margin: 'auto'
+  },
+  TextField: {
+    display: 'block'
+  }
+});
+
+const Form = ({ classes, onSubmit, isLogin }) => {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [err, setError] = useState('');
+
+  const onSubmitClicked = () => {
+    if (username.trim().length === 0 || password.trim().length === 0) {
+      setError('Username or Password should not be blank');
+      return;
+    }
+    onSubmit(username, password);
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.formContainer}>
+           <TextField
+           className={classes.TextField}
+           label="username"
+            value={username}
+            onChange={e => setUserName(e.target.value)}
+            placeholder="User Name"
+            type="text"
+            name="username"
+            required />
+           <TextField
+            className={classes.TextField}
+           label="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Password"
+            type="password"
+            name="password"
+            required />
+            <Button onClick={onSubmitClicked}>{isLogin ? 'Login' : 'Signup'}</Button>
+      </div>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Form);
--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Router from 'next/router';
 import cookie from 'js-cookie';
 import axios from 'axios';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import Form from '../components/Form';
 
 const styles = () => ({
   container: {
@@ -20,15 +18,9 @@ const styles = () => ({
 });
 
 const LoginForm = (props) => {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
   const [err, setError] = useState('');
 
-  const onLoginClicked = async () => {
-    if (username.trim().length === 0 || password.trim().length === 0) {
-      setError('Username or Password should not be blank');
-      return;
-    }
+  const onLoginClicked = async (username, password) => {
     let res;
     try {
       res = await axios.post('http://localhost:3000/api/v1/login', {
@@ -49,36 +41,14 @@ const LoginForm = (props) => {
     }
   };
 
-  const { classes } = props;
-
   return (
-    <div className={classes.container}>
-      <div className={classes.formContainer}>
-           <TextField
-           className={classes.TextField}
-           label="username"
-            value={username}
-            onChange={e => setUserName(e.target.value)}
-            placeholder="User Name"
-            type="text"
-            name="username"
-            required />
-           <TextField
-            className={classes.TextField}
-           label="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            placeholder="Password"
-            type="password"
-            name="password"
-            required />
-            <Button onClick={onLoginClicked}>Submit</Button>
-            {err && (
-              <p>{err}</p>
-            )}
-      </div>
-    </div>
+    <React.Fragment>
+      <Form onSubmit={onLoginClicked} isLogin />
+      {err && (
+        <p>{err}</p>
+      )}
+    </React.Fragment>
   );
 };
 
-export default withStyles(styles)(LoginForm);
+export default LoginForm;
