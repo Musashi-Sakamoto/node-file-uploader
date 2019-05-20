b6b75f24a2ae7591d104b4176f71adcde0983dad
MusashiSakamoto <1292602b@gmail.com>
2019-05-20 18:42:40 +0900

    ポストデータ取得

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ require('./passport');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import cookies from 'next-cookies';
 import Router from 'next/router';
+import axios from 'axios';
 
-const Index = () => (
+const Index = (props) => {
+  const [err, setError] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let res;
+      try {
+        res = await axios.get('http://localhost:3000/api/v1/posts', {
+          headers: {
+            Authorization: `Bearer ${props.token}`
+          }
+        });
+      }
+      catch (error) {
+        if (error.response.status === 401) {
+          Router.push('/login');
+          return;
+        }
+        setError(error.response.data.error.message);
+        return;
+      }
+      const { data } = res;
+      console.log(data.posts);
+    };
+    fetchData();
+  }, [posts, props.token]);
+
+  return (
     <div>
-        <p>Hello Next.js</p>
+      <p>Hello Next.js</p>
+      {err && (
+        <p>{err}</p>
+      )}
     </div>
-);
+  );
+};
 
 Index.getInitialProps = (ctx) => {
   const { token } = cookies(ctx);
@@ -20,7 +53,7 @@ Index.getInitialProps = (ctx) => {
   if (!token) {
     Router.push('/login');
   }
-  return token;
+  return { token };
 };
 
 export default Index;
