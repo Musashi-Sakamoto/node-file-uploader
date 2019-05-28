f2aefef11c194babd3e0ce1da7c1b46fa715ec34
musashi.sakamoto <musashi.sakamoto@brilliantservice.co.jp>
2019-05-28 17:25:18 +0900

    s3バケット使用する

--- backend/utils/s3Control.js
+++ backend/utils/s3Control.js
@@ -1,15 +1,12 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
-const Minio = require('minio');
 const { promisify } = require('util');
 
 aws.config.update({
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   accessKeyId: process.env.ACCESS_KEY_ID,
   region: 'ap-northeast-1',
-  endpoint: process.env.STORAGE_ENDPOINT,
-  s3ForcePathStyle: true,
   signatureVersion: 'v4'
 });
 
@@ -24,18 +21,9 @@ const getS3SignedUrl = async (key, op = 'getObject') => {
   return getSignedUrlAsync(op, params);
 };
 
-const s3ForMinio = new aws.S3();
-s3ForMinio.config.update({
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  region: 'ap-northeast-1',
-  endpoint: process.env.STORAGE_ENDPOINT_MINIO,
-  s3ForcePathStyle: true,
-  signatureVersion: 'v4'
-});
 const upload = multer({
   storage: multerS3({
-    s3: s3ForMinio,
+    s3,
     bucket: process.env.BUCKET_NAME,
     acl: 'public-read',
     metadata(req, file, cb) {
@@ -48,19 +36,11 @@ const upload = multer({
 });
 
 const deleteS3Object = (key) => {
-  const minioClient = new Minio.Client({
-    endPoint: process.env.ENDPOINT_MINIO,
-    port: 9000,
-    useSSL: false,
-    accessKey: process.env.ACCESS_KEY_ID,
-    secretKey: process.env.SECRET_ACCESS_KEY
-  });
   const params = {
     Bucket: process.env.BUCKET_NAME,
     Key: key
   };
-  const removeObjectAsync = promisify(minioClient.removeObject.bind(minioClient));
-  return removeObjectAsync(params.Bucket, params.Key);
+  return s3.deleteObject(params).promise();
 };
 
 module.exports = {
