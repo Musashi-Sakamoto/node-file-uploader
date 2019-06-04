9e795fa4ba2c142eb0f924632ce355e200677acb
musashi.sakamoto <musashi.sakamoto@brilliantservice.co.jp>
2019-06-04 11:58:35 +0900

    編集と削除

--- end-to-end/cypress/integration/main.spec.js
+++ end-to-end/cypress/integration/main.spec.js
@@ -1,4 +1,4 @@
-describe('ログイン画面', () => {
+describe('メイン画面', () => {
     before(() => {
         cy.clearCookies()
         cy.visit('/login')
@@ -24,5 +24,26 @@ describe('ログイン画面', () => {
         cy.get('li').first().contains('added title')
         cy.contains('added title').click()
         cy.get('ul').contains('added description')
+    })
+
+    it('編集', () => {
+        cy.scrollTo(0, 0)
+        cy.get('ul>li>button').eq(1).click()
+        cy.get(`input[id='title']`).should('have.value', 'added title')
+        cy.get(`textarea[id='description']`).should('have.value', 'added description')
+        cy.get(`input[id='title']`).clear().type('added title2')
+        cy.get(`textarea[id='description']`).clear().type('added description2')
+        cy.get('button').contains('Edit').click();
+        cy.get('li').first().contains('added title2')
+        cy.contains('added title2').click()
+        cy.get('ul').contains('added description2')
+    })
+
+    it('削除', () => {
+        cy.scrollTo(0, 0)
+        cy.get('ul>li>button').first().click()
+        cy.get('button').contains('OK').click()
+        cy.get(`input[id='title']`).should('not.have.value', 'added title2')
+        cy.contains('added title2 has been deleted')
     })
 })
