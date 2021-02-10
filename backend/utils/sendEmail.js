17f3c9ca1ee090ee2602bd982803db36a95bf719
Musashi Sakamoto <1292602b@gmail.com>
2021-02-09 21:52:58 -0800

    emailを環境変数に

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 
 const sgMail = require('@sendgrid/mail');
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendVerificationEmail = (to, token) => {
   const { HOST_URL } = process.env;
   const msg = {
     to,
-    from: 'no-reply@example.com', // Use the email address or domain you verified above
+    from: process.env.FROM_EMAIL, // Use the email address or domain you verified above
     subject: 'Verify Your Email',
     text: `Click on this link to verify your email ${HOST_URL}/verification?token=${token}&email=${to}`,
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
   };
   return sgMail.send(msg);
 };
