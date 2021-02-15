49cc0871a2bd8b325fc061a2db69359f02ffc75f
Musashi Sakamoto <1292602b@gmail.com>
2021-02-15 10:20:10 -0800

    developmentを消す

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,9 +6,6 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 'max',
     autorestart: true,
-    env: {
-      NODE_ENV: 'development'
-    },
     env_production: {
       NODE_ENV: 'production'
     }
